feca18d577cd12c9b8c6482023023491c2f063ce
kiyotamonica <monica.kiyota@gmail.com>
2024-04-26 16:12:15 -0300

    feat: operadores aritméticos :sparkles:

---
+++ operadoresAritmeticos.py
@@ -0,0 +1,20 @@
+soma = 10 + 10
+print(soma)
+
+subtracao = 10 - 10
+print(subtracao)
+
+divisao = 10 / 2   #sempre vai retornar float
+print(divisao)
+
+multiplicacao = 10 * 2 #também sempre retorna float
+print(multiplicacao)
+
+exponenciacao = 2 ** 10
+print(exponenciacao)
+
+divisao_inteira = 10 // 3 #retorna somente o valor inteiro
+print(divisao_inteira)
+
+divisao_resto = 10 % 3 #retorna o resto da divisão
+print(divisao_resto)
